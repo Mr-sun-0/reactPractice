4b0c4ee254cae9785947f23c0b90d44c8b9203c2
Gaotian <2240774737@qq.com>
2022-04-04 09:44:54 +0800

    fix: 修改请求信息

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function fetchData() {
+    const url = new Request(`https://bill-processer.qiankun.workers.dev`);
+    fetch(url).then(res=>{
+      console.log(res)
+    }).catch(err=>{
+      console.log(err)
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>请求回来的信息是：{}</p>
+      <button onClick={fetchData}>请求数据</button>
     </div>
   );
 }
